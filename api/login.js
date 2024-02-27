aca303e97ba32f5408cc34b9b6335f24972f4a23
jpsullivan9 <sullivanjames231@gmail.com>
2024-02-27 14:03:59 -0500

    Fixes

--- api/login.js
+++ api/login.js
@@ -13,11 +13,11 @@ module.exports = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const { rows } = await pool.query('SELECT user_id, password_hash FROM accounts WHERE email = $1', [email]);
+    const { rows } = await pool.query('SELECT user_id, username, password_hash FROM accounts WHERE email = $1', [email]);
     if (rows.length > 0) {
       const isValid = await bcrypt.compare(password, rows[0].password_hash);
       if (isValid) {
-        res.status(200).json({ userId: rows[0].user_id, message: "Login successful." });
+        res.status(200).json({ userId: rows[0].user_id, username: rows[0].username, message: "Login successful." });
       } else {
         res.status(401).json({ error: "Invalid credentials." });
       }
