5abfd54837b34ccf9c645ad433c0a804cf8b61f5
Muralii Krishnan Thirumalai <muraliikrishnant@gmail.com>
2024-03-17 19:48:31 -0400

    Change single-quote to double-quote

--- api/search.js
+++ api/search.js
@@ -1,5 +1,5 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
