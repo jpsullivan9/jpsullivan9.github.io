c9377340fe6cba5539bab8cbb16f6d70372db5c3
jpsullivan9 <sullivanjames231@gmail.com>
2024-03-21 13:51:34 -0400

    Added review feature, which is displayed on search page

--- public/js/search.js
+++ public/js/search.js
@@ -50,6 +50,7 @@ function displaySearchResults(data) {
                 <h2>${product.name}</h2>
                 <p>${product.description}</p>
                 <p>Price: $${product.price}</p>
+                ${product.average_review_score !== null ? `<p>Average Reviews: ${product.average_review_score}</p>` : ''}
             </div>
         `).join('');
     }
