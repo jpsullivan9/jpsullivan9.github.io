85cc6bb3ca34c2302f2b29ebb50974c6df2b93cd
randomcrud <157330900+randomcrud@users.noreply.github.com>
2024-03-25 17:34:45 -0400

    Create addressValidation.js

---
+++ api/addressValidation.js
@@ -0,0 +1,42 @@
+const EasyPost = require('@easypost/api');
+const client = new EasyPost(process.env.EasyPost_SECRET_KEY);
+require("dotenv").config();
+
+//hardcoding for the sake of testing, will change later
+const addressParams = {
+    verify: true,
+    street1: '1353 Stelton Rd',
+    city: 'Piscataway',
+    state: 'NJ',
+    zip: '08854',
+    country: 'US',
+    email: 'fourseasonsthai@gmail.com',
+    phone: '+49 497 310 2759'
+};
+
+module.exports = async (req, res) =>{
+
+    try{
+        
+        await client.Address.create(addressParams)
+        .then(address => {
+           // console.log(address);
+            if (address.verifications && address.verifications.delivery && address.verifications.delivery.success) {
+               isValidAddress= true;
+              //console.log('Address verification success');
+                res.status(200).json({message : 'address verified successfully'});
+            } else {
+               res.status(400).json*({message : 'address verification failed'});
+                //console.log('Address verification failed');
+            }
+        })
+        .catch(error => {
+            res.status(400).json({message :  'error creating address', details : error.message});
+            console.error('Error creating address:', error);
+        });
+    }
+    catch(error) {
+        res.status(500).json({message : "can't verify address"});
+    }
+
+};
