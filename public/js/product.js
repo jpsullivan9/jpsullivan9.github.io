2c82e3954ab1b099bdf2537281793c4a526b6ac7
Muralii Krishnan Thirumalai <muraliikrishnant@gmail.com>
2024-04-23 19:54:34 -0400

    Add in/out/low stock code and image size fix

--- public/js/product.js
+++ public/js/product.js
@@ -54,7 +54,7 @@ const displayDetailPage = async (pid) => {
     let entry = 0;
     const detailContent = `
         <div class="row justify-content-md-center p-2">
-            <div class="col-4">
+            <div class="col-md-12 col-lg-4">
                 <div id="prodImages" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         ${imageUrls.map(iurl => {
@@ -65,7 +65,7 @@ const displayDetailPage = async (pid) => {
                             };
                             return `
                                 <div class="carousel-item ${active}">
-                                    <img src="${iurl}" class="w-100" alt="${product.name}"/>
+                                    <img src="${iurl}" style="width: 600px; height: 600px" alt="${product.name}"/>
                                 </div>
                             `;
                         })};
@@ -80,14 +80,16 @@ const displayDetailPage = async (pid) => {
                     </button>
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-md-12 col-lg-6">
                 <p class="fs-2">
                     ${product.name}
                     ${product.featured ? `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M234.7 42.7L197 56.8c-3 1.1-5 4-5 7.2s2 6.1 5 7.2l37.7 14.1L248.8 123c1.1 3 4 5 7.2 5s6.1-2 7.2-5l14.1-37.7L315 71.2c3-1.1 5-4 5-7.2s-2-6.1-5-7.2L277.3 42.7 263.2 5c-1.1-3-4-5-7.2-5s-6.1 2-7.2 5L234.7 42.7zM46.1 395.4c-18.7 18.7-18.7 49.1 0 67.9l34.6 34.6c18.7 18.7 49.1 18.7 67.9 0L529.9 116.5c18.7-18.7 18.7-49.1 0-67.9L495.3 14.1c-18.7-18.7-49.1-18.7-67.9 0L46.1 395.4zM484.6 82.6l-105 105-23.3-23.3 105-105 23.3 23.3zM7.5 117.2C3 118.9 0 123.2 0 128s3 9.1 7.5 10.8L64 160l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L128 160l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L128 96 106.8 39.5C105.1 35 100.8 32 96 32s-9.1 3-10.8 7.5L64 96 7.5 117.2zm352 256c-4.5 1.7-7.5 6-7.5 10.8s3 9.1 7.5 10.8L416 416l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L480 416l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L480 352l-21.2-56.5c-1.7-4.5-6-7.5-10.8-7.5s-9.1 3-10.8 7.5L416 352l-56.5 21.2z"/></svg>` : ""}
                 </p>
                 <p class="fs-4">$${product.price}</p>
                 <p class="fs-6">${product.description}</p>
-                <p><span class="badge text-bg-danger">LOW STOCK</span></p>
+                ${product.stock_quantity <= 0 ? "<p><span class='badge text-bg-danger'>OUT OF STOCK</span></p>" : ""}
+                ${product.stock_quantity > 0 && product.stock_quantity <= 5 ? "<p><span class='badge text-bg-warning'>LOW STOCK</span></p>" : ""}
+                ${product.stock_quantity > 5 ? "<p><span class='badge text-bg-success'>IN STOCK</span></p>" : ""}
                 <p>
                     <button type="button" class="btn btn-dark" ${product.stock_quantity > 0 ? "" : "disabled"}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96zM252 160c0 11 9 20 20 20h44v44c0 11 9 20 20 20s20-9 20-20V180h44c11 0 20-9 20-20s-9-20-20-20H356V96c0-11-9-20-20-20s-20 9-20 20v44H272c-11 0-20 9-20 20z"/></svg>
