73ad4565283dbcb830d428bf3d0b9e468c49a895
randomcrud <157330900+randomcrud@users.noreply.github.com>
2024-04-26 15:10:10 -0400

    add token checking

--- api/delete_save_for_later.js
+++ api/delete_save_for_later.js
@@ -12,13 +12,47 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
+const tokenInfo = async (token) =>{
+   try{
+      const response = await fetch(`${domain}/api/userTokenInfo`, {
+         method : 'POST',
+         headers : {
+            'Content-Type' : 'application/json',
+      },
+        body :  JSON.stringify({token : token}),                          
+      });
+      const data = await response.json();
+   return data;
+      
+   }catch{
+      return false;
+   }
+
+}
 
 module.exports = async (req, res) => {
-    const {user_id, productID} = req.body;
+    const {productID} = req.body;
 
 
 
     try{    
+      
+      const header = req.headers['authorization'];
+       let token;
+      if(typeof header !== 'undefined'){
+        const bearer = header.split(' ');
+        token = bearer[1];
+      //const isValid = await tokenInfo(token);
+
+         // res.status(200).json({token : token, valid : isValid});
+         // req.token = token;
+      } else{
+            res.status(403).json({message : "No token"});
+      }
+      const isValid = await tokenInfo(token);
+ 
+     user_id = isValid.userID;
+      
       cartId = await pool.query('SELECT "cart_id" FROM carts WHERE "user_id" = $1', [user_id]);
     //operation can only be performed on cart items, no input validation     
 
@@ -26,7 +60,7 @@ module.exports = async (req, res) => {
      // res.status(200).json({message : "item successfully added to save for later!", cartId : cartId.rows[0].cart_id, cartId_array : cartId});
 
         const update = await pool.query('UPDATE cart_items SET save_for_later = $1 WHERE cart_id = $2 AND product_id = $3', [false, cartId.rows[0].cart_id, productID]);
-        res.status(200).json({message : "item successfully added to save for later!", cartId : cartId.rows[0].cart_id, productid : productID});
+        res.status(200).json({message : "item successfully added to save for later!"});
 
     }catch(error){
     res.status(500).json({message: "Failure adding to save for later", details : error});
