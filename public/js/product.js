182981b61626f22c84b545cb22ea150066f096e4
Muralii Krishnan <155484676+MuraliiKrishnan@users.noreply.github.com>
2024-04-26 22:52:51 -0400

    Provide visual loading indicator

--- public/js/product.js
+++ public/js/product.js
@@ -13,6 +13,8 @@ const fetchProducts = async (isDetail, id) => {
 };
 
 const buildProductDetail = (cid, scid, pid) => {
+    const spin = document.getElementById(`spin${pid}`);
+    spin.classList.remove("d-none");
     prepHash("cat", cid);
     prepHash("subCat", scid);
     currentPageId = "prod";
@@ -31,12 +33,15 @@ const displayFeaturedProducts = async () => {
 
         // Generate HTML for each featured product
         const featuredProductHTML = products.map(product => `
-            <section onclick="buildProductDetail('${product.cid}', '${product.scid}', '${product.id}')">
+            <section class="cardslot p-3" onclick="buildProductDetail('${product.cid}', '${product.scid}', '${product.id}')">
                 <div class="product">
                     <img src="${noImageUrl(product.image_url)}" alt="${product.name}">
                     <h3>${product.name}</h3>
                     <p>${getNoDescContent(product.description)}</p>
                     <p>Price: $${product.price}</p>
+                    <div id="spin${product.id}" class="spinner-grow d-none" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
                 </div>
             </section>
         `).join('');
@@ -60,6 +65,10 @@ const displayRatings = (rating) => {
 };
 
 const displayDetailPage = async (pid) => {
+    const eye = document.getElementById(`eye${pid}`);
+    const spin = document.getElementById(`spin${pid}`);
+    if (eye) eye.classList.add("d-none");
+    if (spin) spin.classList.remove("d-none");
     const product = await fetchProducts(true, pid);
     let imageUrls = product.image_url.split(",");
     let entry = 0;
@@ -128,9 +137,12 @@ const displayListingPage = async (id) => {
             <div class="col-md-3 col-xl-4"><h4>${product.name}</h4></div>
             <div class="col-sm-6 col-md-3 col-xl-3">$${product.price}</div>
             <div class="col-sm-6 col-md-2 col-xl-2">
-                <a href="#" onclick="displayDetailPage('${product.id}')">
+                <span id="eye${product.id}" style="cursor: pointer" onclick="displayDetailPage('${product.id}')">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
-                </a>
+                </span>
+                <div id="spin${product.id}" class="spinner-grow d-none" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
             </div>
         </div>`;
     });
