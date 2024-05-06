5b9e6f6b7f6775d8a48a0281c96a0cf838bf8d6a
randomcrud <157330900+randomcrud@users.noreply.github.com>
2024-05-05 23:49:43 -0400

    Update remove_quantity.js

--- api/remove_quantity.js
+++ api/remove_quantity.js
@@ -1,5 +1,6 @@
 const {Pool} = require("pg");
 require("dotenv").config();
+const domain  = 'https://rutgers-swe-project.vercel.app';
 
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
@@ -8,11 +9,49 @@ const pool = new Pool({
     }
   });
 
+  const tokenInfo = async (token) =>{
+    try{
+       const response = await fetch(`${domain}/api/userTokenInfo`, {
+          method : 'POST',
+          headers : {
+             'Content-Type' : 'application/json',
+       },
+         body :  JSON.stringify({token : token}),                          
+       });
+       const data = await response.json();
+    return data;
+       
+    }catch{
+       return false;
+    }
+ 
+ }
+
 
   module.exports = async(req, res) => {
-    const {user_id, productID} = await req.body;
+    const {productID} = await req.body;
 
     try {
+          const header = req.headers['authorization'];
+          let token;
+        if(typeof header !== 'undefined'){
+          const bearer = header.split(' ');
+          token = bearer[1];
+        //const isValid = await tokenInfo(token);
+
+            // res.status(200).json({token : token, valid : isValid});
+            // req.token = token;
+        } else{
+              res.status(403).json({message : "No token"});
+        }
+        const isValid = await tokenInfo(token);
+
+        user_id = isValid.userID;
+
+        if(user_id == undefined){
+          res.status(400).json({verified : false, message : "failure"});
+
+        }
           const cartId = await pool.query('SELECT "cart_id" FROM carts WHERE "user_id" = $1', [user_id]);
         const product = await pool.query('SELECT * FROM cart_items WHERE cart_id = $1 AND product_id = $2', [cartId.rows[0].cart_id, productID]);
                     //res.status(200).json({message : "Quantity of item decreased by 1!", cart_item : product.rows[0].quantity});
