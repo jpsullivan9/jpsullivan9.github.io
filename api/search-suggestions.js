e2dcecb56f581aab6186faa93b07f2fa3e007072
jpsullivan9 <sullivanjames231@gmail.com>
2024-05-05 21:16:34 -0400

    Changed empty suggestions to 404 response code in /search-suggestions

--- api/search-suggestions.js
+++ api/search-suggestions.js
@@ -9,14 +9,19 @@ const pool = new Pool({
 const getSuggestions = async (req, res) => {
   const { q } = req.query;
   try {
-    const queryText = `SELECT name FROM products WHERE name ILIKE $1 LIMIT 10;`; // Limit suggestions for performance
+    const queryText = `SELECT name FROM products WHERE name ILIKE $1 LIMIT 10;`;
     const queryParams = [`%${q}%`];
     const { rows } = await pool.query(queryText, queryParams);
     const suggestions = rows.map(row => row.name);
-    res.status(200).json(suggestions);
+
+    if (rows.length === 0) {
+      res.status(404).json(suggestions);
+    } else {
+      res.status(200).json(suggestions);
+    }
   } catch (error) {
     console.error('Error fetching search suggestions:', error);
-    res.status(500);
+    res.status(500).json({ message: 'Failed to fetch search suggestions' });
   }
 };
 
